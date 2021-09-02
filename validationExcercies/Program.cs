b666efb6aa9a5697e1076e8eed5b385374d560cd
Domagoj <Domagoj@DESKTOP-JUAQTUQ>
2021-09-02 13:06:26 +0200

    Add project files.

---
+++ validationExcercies/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace validationExcercies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter number between 1 and 10.");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            if (userNumber >= 1 && userNumber <= 10)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+
+        }
+    }
+}
