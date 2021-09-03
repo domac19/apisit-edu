ba750fa7bfaa047ef4dedff6d86f4df3fa0aa9e9
Domagoj Licitar <75831354+domac19@users.noreply.github.com>
2021-09-03 14:39:14 +0200

    Add files via upload

---
+++ counterNumbers/counterNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace counterNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 100;
+
+            for (int i = 1; i <= length; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    length++;
+                }
+                Console.WriteLine(length);
+            }
+        }
+    }
+}
