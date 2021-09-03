8e7feeadb9b24d9335060979e2b5181dbe44c75c
Domagoj Licitar <75831354+domac19@users.noreply.github.com>
2021-09-03 14:56:24 +0200

    Add files via upload

---
+++ askQuestions/askQuestions/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace askQuestions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter number or ok to exit");
+
+            string enterNumber = Convert.ToString(Console.ReadLine());
+
+            while (enterNumber =="")
+            {
+                Console.WriteLine("Please enter number or ok to exit");
+                if (enterNumber == "1")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
