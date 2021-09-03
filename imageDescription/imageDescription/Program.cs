d2f60b60e38307344cc97d7894378dd252dcfba7
Domagoj Licitar <75831354+domac19@users.noreply.github.com>
2021-09-03 13:03:40 +0200

    Add files via upload

---
+++ imageDescription/imageDescription/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace imageDescription
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Please enter width and height of your image!");
+
+            int imageWidth = Convert.ToInt32(Console.ReadLine());
+            int imageHeight = Convert.ToInt32(Console.ReadLine());
+
+            if (imageWidth > imageHeight)
+            {
+                Console.WriteLine("Your picture is landscape!");
+            }
+            else
+            {
+                Console.WriteLine("Your picture is portrait!");
+            }
+        }
+    }
+}
