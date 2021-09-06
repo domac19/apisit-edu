d2c19ea1d5620b6387d2b092b878cb000330e09d
Domagoj Licitar <75831354+domac19@users.noreply.github.com>
2021-09-06 08:24:25 +0200

    Add files via upload

---
+++ calculateNumbers/calculateNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace calculateNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int enterSomething = 0;
+            
+            while(enterSomething == 0)
+            {
+                Console.WriteLine("Enter a number or ok to exit!");
+                var writeData = Console.ReadLine();
+
+                if (Convert.ToInt32(writeData) > 0)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
