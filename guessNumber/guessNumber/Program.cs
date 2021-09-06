26cbd2e1d4835917730367e189139f5efd5b70bc
Domagoj Licitar <75831354+domac19@users.noreply.github.com>
2021-09-06 10:17:12 +0200

    Add files via upload

---
+++ guessNumber/guessNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace guessNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Secret number 5");
+            
+            Random randomNumbers = new Random();
+            int guessNumber = randomNumbers.Next(1, 11);
+
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i < guessNumber; i++)
+            {
+                Console.WriteLine(guessNumber);
+            }
+            Console.WriteLine("Wrong number!");
+        }
+    }
+}
