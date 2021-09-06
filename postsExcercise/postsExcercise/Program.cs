8a0b79ead8adcc34cc8284dc4df984691a3c47be
Domagoj Licitar <75831354+domac19@users.noreply.github.com>
2021-09-06 13:58:04 +0200

    Add files via upload

---
+++ postsExcercise/postsExcercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace postsExcercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter different names or press Enter and exit! ");
+
+                var inputText = Console.ReadLine();
+                if (inputText == "")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
