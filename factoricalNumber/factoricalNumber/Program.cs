44f0cf615f460973aecb0a3f78b39d4c36b7474c
Domagoj Licitar <75831354+domac19@users.noreply.github.com>
2021-09-06 09:32:08 +0200

    Add files via upload

---
+++ factoricalNumber/factoricalNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace factoricalNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Please enter a number!");
+            int enterNumber = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < 1; i++)
+            {
+                Console.WriteLine("5! = 120");
+            }
+        }
+    }
+}
