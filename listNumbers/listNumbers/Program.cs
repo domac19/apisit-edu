d6b2c72666cff82a0e6a9ca32c947a4fbed5c875
Domagoj Licitar <75831354+domac19@users.noreply.github.com>
2021-09-07 12:27:25 +0200

    Add files via upload

---
+++ listNumbers/listNumbers/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace listNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] displayNumbers;
+            var inputNumbers = Console.ReadLine();
+        }
+    }
+}
