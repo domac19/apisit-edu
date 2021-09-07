faa065e12b65126858b3fd55911b6265909dfee4
Domagoj Licitar <75831354+domac19@users.noreply.github.com>
2021-09-07 08:46:15 +0200

    Add files via upload

---
+++ reverseName/reverseName/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace reverseName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter your name here!");
+            string enterName = Console.ReadLine();
+
+            char[] arrayNames = enterName.ToCharArray();
+            Array.Reverse(arrayNames);
+
+            Console.WriteLine(new String(arrayNames));
+
+        }
+    }
+}
