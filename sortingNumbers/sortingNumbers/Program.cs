31fec91984bd5ccbc4b02b14e07b29336d989946
Domagoj Licitar <75831354+domac19@users.noreply.github.com>
2021-09-07 09:55:28 +0200

    Add files via upload

---
+++ sortingNumbers/sortingNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace sortingNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arrayNumbers = new int[5];
+            Console.WriteLine("Enter five numbers!");
+            int enterNumbers = Convert.ToInt32(Console.ReadLine());
+            
+            while(arrayNumbers.Length > 1)
+            {
+                Console.WriteLine("Error! Try again!");                             
+            }
+        }
+    }
+}
