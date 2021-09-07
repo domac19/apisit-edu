b44a67c21274dc9fae2484987a7d6a35827adc3d
Domagoj Licitar <75831354+domac19@users.noreply.github.com>
2021-09-07 10:23:44 +0200

    Add files via upload

---
+++ uniqueNumbers/uniqueNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace uniqueNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var numberList = new List<string> { };
+
+                Console.WriteLine("Enter number or type QUIT to exit");
+                var enterNumber = Console.ReadLine();
+                numberList.Add(enterNumber);
+                
+                if(enterNumber == "QUIT" || enterNumber.Length >= 1)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
