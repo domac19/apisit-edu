fe474e99ae357ec53d1a3c007b695725cb2623c1
Domagoj Licitar <75831354+domac19@users.noreply.github.com>
2021-09-08 10:58:54 +0200

    Add files via upload

---
+++ hyphenExcercise/hyphenExcercise/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace hyphenExcercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool checkResult = true;
+
+            Console.Write("Enter few numbers separated by hyphen: ");
+            string[] enterNumbers = Console.ReadLine().Split('-');
+            int[] numbersArray = Array.ConvertAll(enterNumbers, int.Parse);
+
+            for (int i = 1; i < numbersArray.Length; i++)
+            {
+                if (numbersArray[i] + 1 == numbersArray[i - 1] || numbersArray[i] - 1 == numbersArray[i - 1] && checkResult == true)
+                {  
+                    checkResult = true;
+                }
+                else
+                {   
+                    checkResult = false;
+                }
+            }
+            if (checkResult)
+            {
+                Console.WriteLine("The Numbers are consecutive");
+            }
+            else
+            {
+                Console.WriteLine("They are not consecutive");
+            }
+        }
+    }
+}
