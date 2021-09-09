d0f7234ac6646a152f90eb0bac62f14141edd24e
Domagoj Licitar <75831354+domac19@users.noreply.github.com>
2021-09-09 11:35:32 +0200

    Add files via upload

---
+++ readingFiles/readingFiles/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace readingFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var textFiles = (@"D:\Excercie\sortingNumbers\sortingNumbers.sln");  
+            
+            var someFiles = File.ReadAllText(textFiles).Length;
+
+           
+
+        }
+    }
+}
