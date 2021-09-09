a0c70e4c9b9b428c5d348b7e1339170aeae1a504
Domagoj Licitar <75831354+domac19@users.noreply.github.com>
2021-09-09 14:38:21 +0200

    Add files via upload

--- seriesNumbers/seriesNumbers/Program.cs
+++ seriesNumbers/seriesNumbers/Program.cs
@@ -8,12 +8,12 @@ namespace seriesNumbers
         {
             Console.WriteLine("Please enter series of numbers and separate it by comma!");
             var seriesNumbers = Console.ReadLine();
+            var separateNumbers = seriesNumbers.Split(',');
 
             for (int i = 0; i < seriesNumbers.Length; i++)
             {
                 Console.WriteLine(seriesNumbers);
             }
-
         }
     }
 }
