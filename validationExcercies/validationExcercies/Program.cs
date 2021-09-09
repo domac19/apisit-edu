67d5f54b437a57d800f37af19030037335f928cc
Domagoj Licitar <75831354+domac19@users.noreply.github.com>
2021-09-09 13:12:07 +0200

    Add files via upload

---
+++ validationExcercies/validationExcercies/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace validationExcercies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter number between 1 and 10.");
+            var userNumber = Convert.ToInt32(Console.ReadLine());
+            var convertInput = Convert.ToInt32(userNumber);
+            
+            if (convertInput >= 1 && convertInput <= 10)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+
+        }
+    }
+}
