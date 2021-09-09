05dba12dc822060c7459847d37657568abf862db
Domagoj Licitar <75831354+domac19@users.noreply.github.com>
2021-09-09 08:49:56 +0200

    Add files via upload

---
+++ pascalCase/pascalCase/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace pascalCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter few words separated by space!");
+            char [] enterWords = Console.ReadLine().ToCharArray();
+            enterWords[0] = char.ToUpper(enterWords[0]);
+            
+            Console.WriteLine(enterWords);
+        }
+    }
+}
