55f31904940738cc9b63ec899d4539d3e527466d
Domagoj Licitar <75831354+domac19@users.noreply.github.com>
2021-09-09 10:03:50 +0200

    Add files via upload

---
+++ englishWords/englishWords/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace englishWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please write an English word!");
+            char[] englishWord = Console.ReadLine().ToCharArray();
+
+            Console.Write(englishWord.Length);
+        }
+    }
+}
