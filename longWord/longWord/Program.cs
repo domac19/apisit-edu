77f0de2db09f798d99371c55c91ad5f72696d57f
Domagoj Licitar <75831354+domac19@users.noreply.github.com>
2021-09-10 11:40:56 +0200

    Add files via upload

---
+++ longWord/longWord/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace longWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var allText = File.ReadAllText(@"D:\document.rtf");
+
+            string[] longWords = allText.Split(new[] { " " }, StringSplitOptions.None);
+            string longWord = "";
+            int numberWords = 0;
+            foreach (String s in longWords)
+            {
+                if (s.Length > numberWords)
+                {
+                    longWord = s;
+                    numberWords = s.Length;
+                }
+            }
+
+            Console.WriteLine(longWord);
+        }
+    }
+}
