acda839f9ee5a0c74f2585e2741b491cc588e333
Domagoj Licitar <75831354+domac19@users.noreply.github.com>
2021-09-15 11:40:56 +0200

    Add files via upload

---
+++ stackPost/stackPost/Post.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace stackPost
+{
+    public class Post
+    {
+        public int counterVotes;
+        private string Title { get; set; }
+        private string Description { get; set; }
+        private DateTime DateTime { get; }
+
+        public int Votes()
+        {
+            return counterVotes;
+        }
+        public void UpVote()
+        {
+            counterVotes++;
+        }
+        public void DownVote()
+        {
+            counterVotes--;
+        }
+    }
+}
---
+++ stackPost/stackPost/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace stackPost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stackPost = new Post();
+            Console.WriteLine("Write your post!");
+            stackPost.UpVote();
+            stackPost.DownVote();
+            Console.WriteLine(stackPost.Votes());
+        }
+    }
+}
