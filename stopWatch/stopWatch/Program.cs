661ecfaab351085cd19a02cb30a768f83f249a8c
Domagoj Licitar <75831354+domac19@users.noreply.github.com>
2021-09-15 08:46:25 +0200

    Add files via upload

---
+++ stopWatch/stopWatch/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace stopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            Thread.Sleep(1000);
+            stopWatch.Stop();
+            TimeSpan timeSpan = stopWatch.elapsedTime;
+
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds,
+            timeSpan.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
+        }
+    }
+}
---
+++ stopWatch/stopWatch/Stopwatch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace stopWatch
+{
+    public class Stopwatch
+    {
+        public DateTime startTime { get; set; }
+        public DateTime stopTime { get; set; }
+        public TimeSpan elapsedTime { get; }
+
+        public void Start()
+        {
+            var startTime = DateTime.Now;
+        }
+        public void Stop()
+        {
+            var stopTime = DateTime.Now;
+        }
+    }
+}
