b1bbd836d301f113a589c0b762582ee142388824
Domagoj Licitar <75831354+domac19@users.noreply.github.com>
2021-09-16 15:31:24 +0200

    Add files via upload

---
+++ Second-Course/stopWatch/stopWatch/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace stopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = new Stopwatch();
+            Console.WriteLine("To start StopWatch you need to hit Enter and to stop hit Esc button, for quit press q !");
+            while (true)
+            {
+                ConsoleKeyInfo keyPress = Console.ReadKey();
+
+                if (keyPress.Key == ConsoleKey.Q)
+                {
+                    break;
+                }
+                else if (keyPress.Key == ConsoleKey.Escape)
+                {
+                    stopWatch.Stop();
+                    Console.WriteLine(" Elapsed Time " + stopWatch.elapsedTime);
+                }
+                else if (keyPress.Key == ConsoleKey.Enter)
+                {
+                    stopWatch.Start();
+                    System.Console.WriteLine("Stopwatch start counting.");
+                }
+            }
+        }
+    }
+}
---
+++ Second-Course/stopWatch/stopWatch/Stopwatch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace stopWatch
+{
+    public class Stopwatch
+    {
+        public DateTime startTime;
+        public DateTime stopTime;
+        public bool isCounting;
+        public TimeSpan elapsedTime => (stopTime - startTime);
+
+        public void Start()
+        {
+            if (isCounting)
+            {
+                throw new Exception("Can not start stopwatch twice in a row!");
+            }
+            else
+            {
+                isCounting = true;
+                startTime = DateTime.Now;
+            }
+        }
+        public void Stop()
+        {
+            if (!isCounting)
+            {
+                throw new Exception("Can not stop stopwatch twice in a row!");
+            }
+            else
+            {
+                isCounting = false;
+                startTime = DateTime.Now;
+            }
+        }
+    }
+}
