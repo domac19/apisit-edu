639bbe085393036ac96d6201ec0c8cc42bd67908
Domagoj Licitar <75831354+domac19@users.noreply.github.com>
2021-09-16 15:33:06 +0200

    Add files via upload

---
+++ Second-Course/stackPost/stackPost/Post.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace stackPost
+{
+    public class Post
+    {
+        public int counterVotes;
+        public string Title { get; set; }
+        public string Description { get; set; }
+        private DateTime DateTime { get; }
+
+        public int CurrentVotes()
+        {
+            return counterVotes;
+        }
+        public void UpVote()
+        {
+            counterVotes++;
+        }
+        public void DownVote()
+        {
+            counterVotes--;     
+        }
+    }
+}
