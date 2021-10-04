bc7712867e8d0e2df125a4157e1e2b05b36c86a3
Domagoj Licitar <75831354+domac19@users.noreply.github.com>
2021-10-04 14:56:53 +0200

    Add files via upload

---
+++ Third-course/lambdaExample/lambdaExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace lambdaExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            var evenNumbers = new int[5] {1,2,3,4,5 };
+            Console.WriteLine("This is a list of 5 even numbers!");
+            
+            foreach (var item in evenNumbers)
+            {
+                Func<int, int> num = number => number * item;
+                Console.WriteLine(num(2));
+            }
+
+            Action<string> thank = thankMessage =>
+            {
+                string saluting = $"Hello {thankMessage}";
+                Console.WriteLine(saluting);
+            };
+            thank("thanks for watching!");
+        }
+    }
+}
