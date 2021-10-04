a29bdbe86aabe37e5e326ab03529f3b3c892e37c
Domagoj Licitar <75831354+domac19@users.noreply.github.com>
2021-10-04 13:06:43 +0200

    Add files via upload

---
+++ Third-course/delegatesExample/delegatesExample/Buildings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace delegatesExample
+{
+    public class Buildings
+    {
+        public delegate void Building(Building building);
+
+        public void Add(Building building)
+        {
+            if (building == null)
+            {
+                Console.WriteLine("This building does not exist!");
+            }
+            else
+            {
+                Console.WriteLine("New building added!");
+            }
+        }
+    }
+}
---
+++ Third-course/delegatesExample/delegatesExample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace delegatesExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var someBuildings = new Buildings();
+            Buildings.Building building = someBuildings.Add;
+            someBuildings.Add(building);
+        }
+    }
+}
