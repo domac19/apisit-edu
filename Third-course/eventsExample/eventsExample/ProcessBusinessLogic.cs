b1843f23e29ce56974d6f6d54d7f9dc1b870af1b
Domagoj Licitar <75831354+domac19@users.noreply.github.com>
2021-10-05 09:47:31 +0200

    Add files via upload

---
+++ Third-course/eventsExample/eventsExample/ProcessBusinessLogic.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace eventsExample
+{
+    public class ProcessBusinessLogic
+    {
+        private string _text;
+        private List<int> _numbers = new List<int>();  
+        
+        public delegate void Notify();
+
+        public event Notify ProcessCompleted; 
+
+        public void StartProcess()
+        {
+            if (String.IsNullOrEmpty(_text))
+            {
+                Console.WriteLine("Process Started!");
+            }
+            OnProcessCompleted();
+
+            _numbers.Add(1);
+            _numbers.Add(0);
+            _numbers.Add(1);
+
+            Console.Write("This is the output: ");
+            foreach (var item in _numbers)
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine("");
+        }
+        protected virtual void OnProcessCompleted()
+        {
+            ProcessCompleted?.Invoke();
+        }
+    }
+}
---
+++ Third-course/eventsExample/eventsExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace eventsExample
+{
+    class Program
+    {  
+        static void Main()
+        {
+                var businessLogic = new ProcessBusinessLogic();
+                businessLogic.ProcessCompleted += bl_ProcessCompleted; 
+                businessLogic.StartProcess();
+        }
+        public static void bl_ProcessCompleted()
+        {
+            Console.WriteLine("Process Completed!");
+        }
+    }
+}
