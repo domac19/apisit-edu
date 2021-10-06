04591bbdd9e7e449785770f580a144c28a809a80
Domagoj Licitar <75831354+domac19@users.noreply.github.com>
2021-10-06 09:46:28 +0200

    Add files via upload

---
+++ Third-course/dynamicsExample/dynamicsExample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace dynamicsExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic name = "Domagoj";
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            try
+            {
+                name++;
+                Console.WriteLine(name);
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("This can not be done becouse name is type string " + error);
+            }
+            finally
+            {
+                Console.WriteLine(name);
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            name = 1;
+            name++;
+
+            if (name > 1)
+            {
+                Console.WriteLine($"Finnaly error is fixed and I get number: {name}");
+            }
+        }
+    }
+}
