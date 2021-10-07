d31394448e961fdbde90648590960ca95607d1c6
Domagoj Licitar <75831354+domac19@users.noreply.github.com>
2021-10-07 13:15:54 +0200

    Add files via upload

--- Third-course/genericsExample/genericsExample/Program.cs
+++ Third-course/genericsExample/genericsExample/Program.cs
@@ -8,25 +8,24 @@ namespace genericsExample
     {
         static void Main(string[] args)
         {
-            var listCity = new GenericList<string>("Zagreb, Split, Rijeka, Varaždin");
+            var listCity = new GenericList<string>("There is list of five big cities in Croatia!");
             Console.WriteLine(listCity.Get());
 
-            Console.WriteLine("Lista od 5 velikih gradova u RH!");
             var bigCity = new City<int, string>();
-            Console.WriteLine(bigCity.AddId(1));
-            Console.WriteLine(bigCity.AddValue("Zagreb"));
+            Console.Write(bigCity.AddId(1));
+            Console.WriteLine(bigCity.AddValue(" Zagreb"));
 
-            Console.WriteLine(bigCity.AddId(2));
-            Console.WriteLine(bigCity.AddValue("Rijeka"));
+            Console.Write(bigCity.AddId(2));
+            Console.WriteLine(bigCity.AddValue(" Rijeka"));
 
-            Console.WriteLine(bigCity.AddId(3));
-            Console.WriteLine(bigCity.AddValue("Split"));
+            Console.Write(bigCity.AddId(3));
+            Console.WriteLine(bigCity.AddValue(" Split"));
 
-            Console.WriteLine(bigCity.AddId(4));
-            Console.WriteLine(bigCity.AddValue("Varaždin"));
+            Console.Write(bigCity.AddId(4));
+            Console.WriteLine(bigCity.AddValue(" Varaždin"));
 
-            Console.WriteLine(bigCity.AddId(5));
-            Console.WriteLine(bigCity.AddValue("Osijek"));
+            Console.Write(bigCity.AddId(5));
+            Console.WriteLine(bigCity.AddValue(" Osijek"));
         }
     }
 }
